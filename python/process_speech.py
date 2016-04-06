57940694a32bb47dfac0e98f3aedd2cf99fbf454
ryoung48 <ryoung48@ufl.edu>
2016-04-06 19:40:00 -0400

    Update

--- python/process_speech.py
+++ python/process_speech.py
@@ -9,7 +9,16 @@ foods = pickle.load(open("python/resources/foods.p", "rb"))
 # list of foods: http://www.mrsjonesroom.com/jones/foodalphabet.html
 def process_user_input(text):
     candidates = text.split(" ")
-    return [word for word in candidates if word.lower() in foods and word.lower() not in stopwords]
+    result = dict()
+    count = 1
+    for word in candidates:
+        if word.lower() not in stopwords:
+            if word.isdigit():
+                count = int(word)
+            elif word.lower() in foods:
+                result[word] = count
+                count = 1
+    return result
 
 data = json.loads(sys.argv[1])
 data['list_of_foods'] = process_user_input(data['user_input'])
