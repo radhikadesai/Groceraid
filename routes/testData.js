ea19d721ef73dd53e4a89ffca4fb4e0de93f054f
williamsd4808 <williamsd4808@ufl.edu>
2016-04-13 16:24:14 -0400

    Update to my food test data
    Disregard this, has no impact whatsoever.

--- routes/testData.js
+++ routes/testData.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* You are entering my sandbox. Proceed with caution. */
 router.get('/', function(req, res, next) {
-	var x = {"user_input":"Hello Mr. Fridge today you have milk eggs and cheese","list_of_foods":{"cheese":1,"eggs":2,"milk":3}}; //{'user_input':'Hello Mr. Fridge today you have milk eggs and cheese'}; {'user_input':['chocolate', 'milk']}; {'user_input':'1309 NW 5th Ave, Gainesville, FL'};
 	res.send(JSON.stringify(x));
 });
 
